0e8db7a7f8a2c7e1d8e6df9972f4d2c437722c11
Sonif <sonifdev@gmail.com>
2025-08-20 15:58:09 +0700

    update bar

--- lib/agen/visitasi/visitasi_list.dart
+++ lib/agen/visitasi/visitasi_list.dart
@@ -2,6 +2,7 @@ import 'package:djadol_mobile/core/utils/ext_date.dart';
 import 'package:easy_date_timeline/easy_date_timeline.dart';
 import 'package:flutter/material.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
 
 import '../../core/utils/api_service.dart';
 import '../../core/widgets/flist_page.dart';
@@ -17,6 +18,7 @@ class VisitasiListPage extends StatefulWidget {
 
 class _VisitasiListPageState extends State<VisitasiListPage> {
   DateTime _selectedDate = DateTime.now();
+  int countTotal = 0;
 
   static const _pageSize = 20;
   late final _pagingController = PagingController<int, dynamic>(
@@ -44,6 +46,7 @@ class _VisitasiListPageState extends State<VisitasiListPage> {
 
       List result =
           await ApiService().getList('/all/43', page, _pageSize, data: filter);
+      setState(() {});
       return result.map((i) => Jurnal.fromMap(i)).toList();
     } catch (error) {
       debugPrint('Error fetching data: $error');
@@ -61,7 +64,8 @@ class _VisitasiListPageState extends State<VisitasiListPage> {
         InkWell(
           child: Card(
             child: ListTile(
-              title: Text('${++index}. ${i.retailIdName}'),
+              title: Text('${++index}. ${i.retailIdName}',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
               subtitle: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -74,7 +78,17 @@ class _VisitasiListPageState extends State<VisitasiListPage> {
                     ],
                   ),
                   i.isNew == '1'
-                      ? const Icon(Icons.new_releases, color: Colors.green)
+                      ? Text('New ',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            background: Paint()
+                              ..color = Colors.orange
+                              ..strokeWidth = 20
+                              ..strokeJoin = StrokeJoin.round
+                              ..strokeCap = StrokeCap.round
+                              ..style = PaintingStyle.stroke,
+                            color: Colors.white,
+                          ))
                       : const SizedBox.shrink(),
                 ],
               ),
@@ -112,6 +126,21 @@ class _VisitasiListPageState extends State<VisitasiListPage> {
                 pagingController: _pagingController,
                 itemBuilder: (context, item, index) =>
                     widgetItemList(item, index),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: LinearPercentIndicator(
+                lineHeight: 14.0,
+                percent: 0.5,
+                center: Text(
+                  "${(_pagingController.items?.length ?? 0) / 40 * 100} %",
+                  style: TextStyle(fontSize: 12.0, color: Colors.white),
+                ),
+                trailing: Text('${_pagingController.items?.length ?? 0} /40'),
+                // linearStrokeCap: LinearStrokeCap.roundAll,
+                backgroundColor: Colors.grey,
+                progressColor: Theme.of(context).colorScheme.primary,
               ),
             ),
           ],
